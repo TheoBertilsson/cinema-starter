8b326a24be7fdd270ffe40bd986e93074e0b95c0
Theo Bertilsson <131893433+TheoBertilsson@users.noreply.github.com>
2025-07-20 10:01:12 +0200

    Add: booking list

--- app/routes/bookingStart.tsx
+++ app/routes/bookingStart.tsx
@@ -26,19 +26,20 @@ export default function BookingStart() {
         <h1 className="text-3xl mb-2">Welcome to the cinema!</h1>
         <p>This is where your implementation will start.</p>
       </header>
-      <main className="flex w-full flex-col items-center justify-center gap-4 my-20">
-        {Object.entries(seatMap ? seatMap : []).map(([row, rowSeats]) => (
-          <div key={row}>
-            <div className="flex gap-2 justify-center items-center">
-              {rowSeats.map((status, index) => (
-                <button
-                  key={index}
-                  disabled={status === "Booked"}
-                  onClick={() => {
-                    const updatedSeatMap = selectSeat(row, index);
-                    if (updatedSeatMap) setSeatMap(updatedSeatMap);
-                  }}
-                  className={`
+      <main className="flex w-full flex-col items-center justify-center gap-4 m-5">
+        <article className="flex w-full flex-col items-center justify-center gap-4">
+          {Object.entries(seatMap ? seatMap : []).map(([row, rowSeats]) => (
+            <div key={row}>
+              <div className="flex gap-2 justify-center items-center">
+                {rowSeats.map((status, index) => (
+                  <button
+                    key={index}
+                    disabled={status === "Booked"}
+                    onClick={() => {
+                      const updatedSeatMap = selectSeat(row, index);
+                      if (updatedSeatMap) setSeatMap(updatedSeatMap);
+                    }}
+                    className={`
                     size-8 rounded-b-lg
                     ${
                       status === "Available"
@@ -53,37 +54,50 @@ export default function BookingStart() {
                     ${
                       status === "Booked" ? "bg-red-600 cursor-not-allowed" : ""
                     }`}
-                ></button>
-              ))}
+                  ></button>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
-        <form
-          onSubmit={(e) => {
-            const updatedSeatMap = bookSeats(
-              e,
-              (e.target as HTMLFormElement).bookingName.value || ""
-            );
-            if (updatedSeatMap) {
-              setSeatMap(updatedSeatMap.updatedSeatMap);
-              setBookingList(updatedSeatMap.bookingList);
-            }
-          }}
-          className="flex flex-col gap-2"
-        >
-          <input
-            type="text"
-            placeholder="Name"
-            name="bookingName"
-            className="border border-gray-400 p-2 rounded-lg"
-          />
-          <button
-            className="bg-blue-400 w-full font-bold text-xl text-white p-2 rounded-lg hover:bg-blue-300"
-            type="submit"
+          ))}
+          <form
+            onSubmit={(e) => {
+              const updatedSeatMap = bookSeats(
+                e,
+                (e.target as HTMLFormElement).bookingName.value || ""
+              );
+              if (updatedSeatMap) {
+                setSeatMap(updatedSeatMap.updatedSeatMap);
+                setBookingList(updatedSeatMap.bookingList);
+              }
+            }}
+            className="flex flex-col gap-2"
           >
-            Book Seats
-          </button>
-        </form>
+            <input
+              type="text"
+              placeholder="Name"
+              name="bookingName"
+              className="border border-gray-400 p-2 rounded-lg"
+            />
+            <button
+              className="bg-blue-400 w-full font-bold text-xl text-white p-2 rounded-lg hover:bg-blue-300"
+              type="submit"
+            >
+              Book Seats
+            </button>
+          </form>
+        </article>
+        <section className="flex p-5 border-2 rounded-lg border-gray-500 gap-4 max-w-[37.5rem]  overflow-x-scroll">
+          {bookingList?.map((booking, index) => (
+            <div
+              className=" flex flex-col gap-2 border-r border-gray-600 p-5 min-w-[14rem] min-h-[7rem]"
+              key={index}
+            >
+              <p>Name:{booking.name}</p>
+              <p>Amount of tickets: {booking.seatAmount}</p>
+              <p>Seats: {booking.seatNumber.map((seat) => seat)}</p>
+            </div>
+          ))}
+        </section>
       </main>
     </>
   );
